b118f4f9fcb40bc2af3c2258328e74738364cd6c
Aleksander Seryogin <a.siarohin@gmail.com>
2018-11-07 21:30:31 +0300

    feat: - create function number of points on the page

--- js/youtube-module.js
+++ js/youtube-module.js
@@ -1,4 +1,4 @@
-const maxResult = 3; // set max Results of Elements
+const maxResult = 12; // set max Results of Elements
 
 let valueUser;
 
@@ -23,7 +23,7 @@ let arrID = []; // for Elements ID
   for (let i = 0; i < sliderBlock.pageInfo.resultsPerPage; i++) {
 
     arrID.push(sliderBlock.items[i].id.videoId); // save Elements ID
-    
+
     // date Convert
 
     let dateConvert = sliderBlock.items[i].snippet.publishedAt;
@@ -34,9 +34,11 @@ let arrID = []; // for Elements ID
     let nameConvert = sliderBlock.items[i].snippet.title;
     nameConvert = nameConvert.trim();
 
+    let numberID = i + 1; // set Elements ID
+
     // create Elements block
 
-    document.querySelector('.slider').innerHTML += `<div class="">
+    document.querySelector('.slider').innerHTML += `<div id="${numberID}">
     <img src="${sliderBlock.items[i].snippet.thumbnails.high.url}" alt="">
     <a href="https://www.youtube.com/watch?v=${sliderBlock.items[i].id.videoId}" target="_blank" title="${nameConvert}">${nameConvert}</a>
     <ul>
@@ -45,7 +47,7 @@ let arrID = []; // for Elements ID
     </ul>
     <p>${sliderBlock.items[i].snippet.description}</p>
     </div>`;
-    
+
   }
 
     fetch('https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCAznfTwZKs8R47J-_PkpBrHYaRvcCmKwY&id=' + [arrID].join(',') + '&part=snippet,statistics') // get Elements statistic
@@ -55,6 +57,7 @@ let arrID = []; // for Elements ID
     .then(function(reviewCount) {
 
       for (let i = 0; i < reviewCount.pageInfo.resultsPerPage; i++) {
+
         let li = document.createElement('li');
         document.querySelector('.slider').getElementsByTagName('ul')[i].append(li);
         li.innerHTML = reviewCount.items[i].statistics.viewCount; // create Elements statistic
@@ -67,27 +70,52 @@ let arrID = []; // for Elements ID
 }
 
 
-let pointsItem = `<a class="active " href="" title="">1</a>`;
 
 let wrapper = document.createElement('div');
 
 wrapper.id = 'wrapper';
 wrapper.innerHTML = `<header>
   <div class="search-bar">
-    <form id="search" method="get" onsubmit="searchResult(); return false;">
+    <form id="search" method="get" onsubmit="searchResult(); getPoints(); return false;">
     <input type="search" autofocus="autofocus" autocomplete="off" placeholder="Search">
     </form>
   </div>
 </header>
 <div class="content">
   <div class="slider">
-    
+
   </div>
 </div>
 <footer>
   <div class="points">
-    ${pointsItem}
+   
   </div>
 </footer>`;
 
 document.body.prepend(wrapper);
+
+
+
+
+
+
+function getPoints() {
+  let counterOfBlocks = Math.floor(document.body.querySelector('#wrapper').clientWidth / 320); // number of blocks
+
+  let numberOfPoints = Math.ceil(maxResult / counterOfBlocks); // number of points on the page
+
+
+  for (let i = 1; i < numberOfPoints + 1; i++) {
+    let point = `<a class="active " href="" title="">${i}</a>`;
+    document.querySelector('.points').innerHTML += point;
+  }
+
+}
+
+
+// change number of points on resize window
+
+window.onresize = function() {
+  document.querySelector('.points').innerHTML = '';
+  getPoints();
+}
