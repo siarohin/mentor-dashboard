25345474c138b1fe48cbd637a78b78ee79641736
Aleksander Seryogin <a.siarohin@gmail.com>
2018-11-08 16:59:29 +0300

    feat: - create action a -> div

--- js/youtube-module.js
+++ js/youtube-module.js
@@ -1,95 +1,147 @@
-const maxResult = 12; // set max Results of Elements
+(function createWrapper() {
+  let wrapper = document.createElement('div');
 
-let valueUser; // user search phrase
+  wrapper.id = 'wrapper';
+  wrapper.innerHTML = `<header>
+    <div class="search-bar">
+      <form id="search" method="get" onsubmit="searchResult(); return false;">
+      <input type="search" autofocus="autofocus" autocomplete="off" placeholder="Search">
+      </form>
+    </div>
+  </header>
+  <div class="content">
+    <div class="slider">
 
-const GLOBAL_BLOCK_SETTING = 320; // width of block
+    </div>
+  </div>
+  <footer>
+    <div class="points">
 
-function searchResult() {
+    </div>
+  </footer>`;
 
-let valueUser = document.querySelector('#search')[0].value; // get User request from input
+  document.body.prepend(wrapper);
+})();
 
+// set max Results of Elements
+const maxResult = 12;
 
-fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCAznfTwZKs8R47J-_PkpBrHYaRvcCmKwY&type=video&part=snippet&maxResults=' + maxResult + '&q=' + valueUser) // get Elements result
-.then(function(response) {
-  return response.json();
-})
-.then(function(sliderBlock) {
+// search phrase
+let searchValue;
 
-let arrID = []; // for Elements ID
+// width of block
+const GLOBAL_BLOCK_SETTING = 320;
 
-  for (let i = 0; i < sliderBlock.pageInfo.resultsPerPage; i++) {
-    document.querySelector('.slider').innerHTML = ''; // Reset Elements
-  }
+// for video id
+let arrId = [];
 
-  for (let i = 0; i < sliderBlock.pageInfo.resultsPerPage; i++) {
+// number of blocks
+let counterOfBlocks;
 
-    arrID.push(sliderBlock.items[i].id.videoId); // save Elements ID
+// number of points
+let numberOfPoints;
 
-    let numberID = i + 1; // set ID to block
-
-    // create Elements block
-
-    document.querySelector('.slider').innerHTML += `<div id="${numberID}">
-    <img src="${sliderBlock.items[i].snippet.thumbnails.high.url}" alt="">
-    <a href="https://www.youtube.com/watch?v=${sliderBlock.items[i].id.videoId}" target="_blank" title="${sliderBlock.items[i].snippet.title}">${sliderBlock.items[i].snippet.title}</a>
-    <ul>
-      <li>${sliderBlock.items[i].snippet.channelTitle}</li>
-      <li>${(sliderBlock.items[i].snippet.publishedAt).substring(0,10)}</li>
-    </ul>
-    <p>${sliderBlock.items[i].snippet.description}</p>
-    </div>`;
-
-  }
-
-    fetch('https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCAznfTwZKs8R47J-_PkpBrHYaRvcCmKwY&id=' + [arrID].join(',') + '&part=snippet,statistics') // get Elements statistic
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(reviewCount) {
-
-      for (let i = 0; i < reviewCount.pageInfo.resultsPerPage; i++) {
-
-        let li = document.createElement('li');
-        document.querySelector('.slider').getElementsByTagName('ul')[i].append(li);
-        li.innerHTML = reviewCount.items[i].statistics.viewCount; // create Elements statistic
-      }
-
-    });
-
-    resetPoints(); // reset points
-    drawPoints(); // drawing points
-
-});
-
-function resetPoints() {
-    document.querySelector('.points').innerHTML = '';
+// calculate number of blocks
+function calculateOfBlocks() {
+  return counterOfBlocks = Math.floor(document.body.querySelector('#wrapper').clientWidth / GLOBAL_BLOCK_SETTING);
 }
 
+// calculate number of points
+function calculateNumberOfPoints() {
+  calculateOfBlocks();
+  return numberOfPoints = Math.ceil(maxResult / counterOfBlocks);
+}
+
+// delete sliders
+function deleteSliders() {
+  document.querySelector('.slider').innerHTML = '';
+}
+
+// delete points
+function deletePoints() {
+  document.querySelector('.points').innerHTML = '';
+}
+
+// draw points
 function drawPoints() {
-  let counterOfBlocks = Math.floor(document.body.querySelector('#wrapper').clientWidth / GLOBAL_BLOCK_SETTING); // number of blocks
+  calculateNumberOfPoints();
 
-  let numberOfPoints = Math.ceil(maxResult / counterOfBlocks); // number of points on the page
-
-  resetPoints();
-
-  for (let i = 1; i < numberOfPoints + 1; i++) {
-    let point = `<a class="" onclick="changePoints(this)">${i}</a>`;
+  for (let i = 0; i < numberOfPoints; i++) {
+    let point = `<a href="#${arrId[i]}" class="" onclick="changePoints(this)">${i+1}</a>`;
     document.querySelector('.points').innerHTML += point;
   }
 
   return false;
 }
 
-
-window.onresize = function() {  // change number of points on resize window
-  drawPoints();
+// get user search phrase
+function getSearchValue() {
+  return searchValue = document.querySelector('#search')[0].value;
 }
 
+function searchResult() {
+
+  // get user search phrase
+  getSearchValue();
+
+  // send response to api
+  fetch('https://www.googleapis.com/youtube/v3/search?key=AIzaSyCAznfTwZKs8R47J-_PkpBrHYaRvcCmKwY&type=video&part=snippet&maxResults=' + maxResult + '&q=' + searchValue) // get Elements result
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(sliderBlock) {
+    // delete all sliders blocks
+    deleteSliders();
+    // reset video id
+    arrId.length = 0;
+
+    for (let i = 0; i < sliderBlock.pageInfo.resultsPerPage; i++) {
+      // save video id
+      arrId.push(sliderBlock.items[i].id.videoId);
+
+      // create blocks
+      document.querySelector('.slider').innerHTML += `<div id="${arrId[i]}">
+      <img src="${sliderBlock.items[i].snippet.thumbnails.high.url}" alt="">
+      <a href="https://www.youtube.com/watch?v=${sliderBlock.items[i].id.videoId}" target="_blank" title="${sliderBlock.items[i].snippet.title}">${sliderBlock.items[i].snippet.title}</a>
+      <ul>
+        <li>${sliderBlock.items[i].snippet.channelTitle}</li>
+        <li>${(sliderBlock.items[i].snippet.publishedAt).substring(0,10)}</li>
+      </ul>
+      <p>${sliderBlock.items[i].snippet.description}</p>
+      </div>`;
+    }
+
+      fetch('https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCAznfTwZKs8R47J-_PkpBrHYaRvcCmKwY&id=' + [...arrId] + '&part=snippet,statistics') // get Elements statistic
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(reviewCount) {
+        for (let i = 0; i < reviewCount.pageInfo.resultsPerPage; i++) {
+          let li = document.createElement('li');
+          document.querySelector('.slider').getElementsByTagName('ul')[i].append(li);
+          li.innerHTML = reviewCount.items[i].statistics.viewCount;
+        }
+      });
+
+      deletePoints();
+      drawPoints();
+
+  });
+
 }
 
+// change number of points on resize window
+window.onresize = function() {
+
+  deletePoints();
+
+  // if block has been drawed -> draw points
+  if (document.querySelector('.slider').children.length > 0) {
+    drawPoints();
+  }
+}
 
 // click on points
-
 function changePoints(element) {
   this.element = element;
 
@@ -104,27 +156,3 @@ function changePoints(element) {
 
 	return false;
 }
-
-
-let wrapper = document.createElement('div');
-
-wrapper.id = 'wrapper';
-wrapper.innerHTML = `<header>
-  <div class="search-bar">
-    <form id="search" method="get" onsubmit="searchResult(); return false;">
-    <input type="search" autofocus="autofocus" autocomplete="off" placeholder="Search">
-    </form>
-  </div>
-</header>
-<div class="content">
-  <div class="slider">
-
-  </div>
-</div>
-<footer>
-  <div class="points">
-
-  </div>
-</footer>`;
-
-document.body.prepend(wrapper);
