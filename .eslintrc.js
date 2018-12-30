34baae0bf4f50492f1aea62be274f35f98358b89
Aleksander Seryogin <a.siarohin@gmail.com>
2018-12-31 01:07:22 +0300

    feat: add footer, index.html

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base",
-  
-    "globals": {
-      "window": true,
-       "document": true,
-       "localStorage": true,
-        "fetch": true,
-    },
-      "env": {
-        "browser": true,
-        "node": true,
-        "jest": true,
-      },
-      "rules": {
-        "max-len": ["error", { "code": 80 }],
-    },
-  };
+  "extends": "airbnb-base",
+  "globals": {
+  "window": true,
+  "document": true,
+  "localStorage": true,
+  "fetch": true,
+  },
+  "env": {
+  "browser": true,
+  "node": true,
+  "jest": true,
+  },
+  "rules": {
+  "max-len": ["error", { "code": 80 }],
+  },
+};
--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-# Original project for game bootstrap
-[![Tests are passing](https://img.shields.io/badge/tests-passing-brightgreen.svg)](https://shields.io/)
+# The Monster Game
 
-It's working with simple and clear config!
 
-Check it on your own:
-
-1. `git clone https://github.com/davojta/rss-2018q3-game-bootsrap.git`
-2. `cd rss-2018q3-game-bootsrap`
-3. `npm install`
-4. `npm test`
-5. `npm start`
 
 # Contributors
 
+https://github.com/siarohin
+
 https://github.com/DimaDK24
+
 https://github.com/auseika
+
 https://github.com/davojta
