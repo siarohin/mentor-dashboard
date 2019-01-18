fb96cffd76f100ca5bb7be3cd1835f74644f0250
Aleksander Seryogin <a.siarohin@gmail.com>
2019-01-18 03:06:27 +0300

    feat: add `tasks` workbook, merge `tasks` and `mentorStudentPair`

---
+++ mentor-dashboard/src/mentorStudentsPairs.js
@@ -0,0 +1,144 @@
+const path = require('path');
+const XLSX = require('xlsx');
+const getSheetData = require('./utils');
+
+/* path to raw files with data */
+const pathToFile = path.join(__dirname, './rawSource/Mentor-students pairs.xlsx');
+const readFile = XLSX.readFile(pathToFile);
+
+/* range of rows in xlsx files */
+const START_ROW = 2;
+const LIMIT_ROW = 65535;
+
+
+/* Do we have next row? */
+const getNextRow = (sheet, column, row) => {
+  const nextRow = sheet[column + row];
+  if (nextRow !== undefined) {
+    return true;
+  } return false;
+};
+
+
+const workbook = {
+  // Sheet1
+  interviewer: 'A',
+  studentGithub: 'B',
+
+  // Sheet2
+  mentorName: 'A',
+  mentorSername: 'B',
+  city: 'C',
+  mentorGithub: 'E',
+};
+
+
+// Sheet1 workbook ===============================
+
+const sheet1 = getSheetData(readFile, 1);
+
+const getMentorStudentPair = (currentRow) => {
+  const mentorStudentPair = {
+    interviewer: (sheet1[workbook.interviewer + currentRow].v).toString().trim(),
+    studentGithub: (sheet1[workbook.studentGithub + currentRow].v).toString().trim(),
+  };
+
+  return mentorStudentPair;
+};
+
+const getPairs = () => {
+  const rows = [];
+  for (let i = START_ROW; i < LIMIT_ROW; i += 1) {
+    if (getNextRow(sheet1, workbook.interviewer, i)) {
+      rows.push(i);
+    }
+  }
+  const dataRow = rows.map(row => getMentorStudentPair(row));
+  return dataRow;
+};
+
+const pairs = getPairs();
+
+// Merge rows with mentor names
+const mergeMentorStudentPair = pairs.reduce((acc, item) => {
+  const exsistingMentor = acc.find(accItem => accItem.interviewer === item.interviewer);
+
+  if (exsistingMentor) {
+    exsistingMentor.studentGithub
+      .push({ studentGithub: item.studentGithub });
+
+    exsistingMentor.studentGithub
+      .sort((first, second) => (first.studentGithub).localeCompare(second.studentGithub));
+  } else {
+    acc.push({
+      interviewer: item.interviewer,
+      studentGithub: [
+        { studentGithub: item.studentGithub },
+      ],
+    });
+  }
+  return acc;
+}, []);
+
+
+// Sheet2 workbook ===============================
+
+const sheet2 = getSheetData(readFile, 2);
+
+const getMentorData = (currentRow) => {
+  const mentorData = {
+    mentorFullName: `${sheet2[workbook.mentorName + currentRow].v} ${sheet2[workbook.mentorSername + currentRow].v}`,
+    mentorCity: (sheet2[workbook.city + currentRow].v).toString().trim(),
+    mentorGithub: (sheet2[workbook.mentorGithub + currentRow].v)
+      .toString()
+      .trim()
+      .toLowerCase()
+      .replace('https://github.com/', '')
+      .replace('http://github.com/', '')
+      .replace('/', ''),
+  };
+
+  return mentorData;
+};
+
+const getMentors = () => {
+  const rows = [];
+  for (let i = START_ROW; i < LIMIT_ROW; i += 1) {
+    if (getNextRow(sheet2, workbook.mentorName, i)) {
+      rows.push(i);
+    }
+  }
+  const dataRow = rows.map(row => getMentorData(row));
+  return dataRow;
+};
+
+const mentors = getMentors();
+
+
+// Merge Sheet1 and Sheet2 workbook and sort result ========
+
+const result = mergeMentorStudentPair
+  .map((mentorStudentPair) => {
+    const mentorStudentsPairs = mentors.find(data => data.mentorFullName === mentorStudentPair.interviewer);
+
+    if (!mentorStudentsPairs) {
+      return {
+        mentorName: mentorStudentPair.interviewer || '',
+        mentorCity: '',
+        mentorGithub: '',
+        students: mentorStudentPair.studentGithub || '',
+      };
+    }
+    return {
+      mentorName: mentorStudentsPairs.mentorFullName,
+      mentorCity: mentorStudentsPairs.mentorCity,
+      mentorGithub: mentorStudentsPairs.mentorGithub,
+      students: mentorStudentPair.studentGithub,
+    };
+  });
+
+// Sort result by name
+const resultSortByName = result.sort((first, second) => first.mentorName.localeCompare(second.mentorName));
+
+
+module.exports = resultSortByName;
---
+++ mentor-dashboard/src/tasks.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const XLSX = require('xlsx');
+const getSheetData = require('./utils');
+
+/* path to raw files with data */
+const pathToFile = path.join(__dirname, './rawSource/Tasks.xlsx');
+const readFile = XLSX.readFile(pathToFile);
+
+/* range of rows in xlsx files */
+const START_ROW = 2;
+const LIMIT_ROW = 65535;
+
+
+/* Do we have next row? */
+const getNextRow = (sheet, column, row) => {
+  const nextRow = sheet[column + row];
+  if (nextRow !== undefined) {
+    return true;
+  } return false;
+};
+
+
+const workbook = {
+  // Sheet1
+  name: 'A',
+  link: 'B',
+  status: 'C',
+};
+
+
+// Sheet1 workbook ===============================
+
+const sheet1 = getSheetData(readFile);
+
+const getTask = (currentRow) => {
+  let link = '';
+  if (sheet1[workbook.link + currentRow]) {
+    link = (sheet1[workbook.link + currentRow].v).toString().trim();
+  }
+
+  const task = {
+    name: (sheet1[workbook.name + currentRow].v).toString().trim(),
+    link,
+    status: (sheet1[workbook.status + currentRow].v).toString().trim(),
+  };
+
+  return task;
+};
+
+const getTasks = () => {
+  const rows = [];
+  for (let i = START_ROW; i < LIMIT_ROW; i += 1) {
+    if (getNextRow(sheet1, workbook.name, i)) {
+      rows.push(i);
+    }
+  }
+  const dataRow = rows.map(row => getTask(row));
+  return dataRow;
+};
+
+const tasks = getTasks();
+
+// // Merge rows with mentor names
+// const mergeMentorStudentPair = pairs.reduce((acc, item) => {
+//   const exsistingMentor = acc.find(accItem => accItem.interviewer === item.interviewer);
+
+//   if (exsistingMentor) {
+//     exsistingMentor.studentGithub
+//       .push({ studentGithub: item.studentGithub });
+
+//     exsistingMentor.studentGithub
+//       .sort((first, second) => (first.studentGithub).localeCompare(second.studentGithub));
+//   } else {
+//     acc.push({
+//       interviewer: item.interviewer,
+//       studentGithub: [
+//         { studentGithub: item.studentGithub },
+//       ],
+//     });
+//   }
+//   return acc;
+// }, []);
+
+
+
+
+// // Sort result by name
+// const resultSortByName = result.sort((first, second) => first.mentorName.localeCompare(second.mentorName));
+
+
+module.exports = tasks;
