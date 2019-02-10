d187992cd5923e45f225db04bf09acde53dfeb14
Aleksander Seryogin <a.siarohin@gmail.com>
2019-02-10 23:38:27 +0300

    Updates

--- precache-manifest.e8ec964aeb7a94b18d476d921f62552c.js
+++ precache-manifest.e8ec964aeb7a94b18d476d921f62552c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/mentor-dashboard/static/js/runtime~main.5ad02569.js"
   },
   {
-    "revision": "bfbfc6a355a42b447d7b",
-    "url": "/mentor-dashboard/static/js/main.bfbfc6a3.chunk.js"
+    "revision": "841d2177854e1efaa92a",
+    "url": "/mentor-dashboard/static/js/main.841d2177.chunk.js"
   },
   {
     "revision": "d06abd9d400a2202a288",
     "url": "/mentor-dashboard/static/js/1.d06abd9d.chunk.js"
   },
   {
-    "revision": "bfbfc6a355a42b447d7b",
+    "revision": "841d2177854e1efaa92a",
     "url": "/mentor-dashboard/static/css/main.5c2972a6.chunk.css"
   },
   {
-    "revision": "28550836389b5711afdc597cff2beb9f",
+    "revision": "fc9d09d6486ba6df8d8d84aec1fb8a8e",
     "url": "/mentor-dashboard/index.html"
   }
 ];
