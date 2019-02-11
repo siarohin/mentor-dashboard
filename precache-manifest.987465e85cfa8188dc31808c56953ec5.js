55c08166d842f659d51f4e5636afbe1b9a796d35
Aleksander Seryogin <a.siarohin@gmail.com>
2019-02-12 02:20:07 +0300

    Updates

---
+++ precache-manifest.987465e85cfa8188dc31808c56953ec5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4b8f0c0533d87f7ff34",
+    "url": "/static/js/main.c4b8f0c0.chunk.js"
+  },
+  {
+    "revision": "13b69f2ab1fcf7ed34d9",
+    "url": "/static/js/1.13b69f2a.chunk.js"
+  },
+  {
+    "revision": "c4b8f0c0533d87f7ff34",
+    "url": "/static/css/main.e9346c85.chunk.css"
+  },
+  {
+    "revision": "3cac24e285f78b0f2d87b5fdadd815ed",
+    "url": "/index.html"
+  }
+];
