53c9462109762388c2d2f1787c64f15f6fc5bd87
Aleksander Seryogin <a.siarohin@gmail.com>
2019-02-17 17:45:14 +0300

    feat: add catch to fetch data.json from server

--- mentor-dashboard/react-component/src/containers/App/index.jsx
+++ mentor-dashboard/react-component/src/containers/App/index.jsx
@@ -4,14 +4,18 @@ import Dashboard from '../../components/Dashboard/';
 import { auth } from '../../firebase/';
 import { Preloader } from '../../components/Preloader/';
 import { CONSTANT } from '../../components/constant';
+import Layout from '../Layout/';
 
 const { sourceJsonURL, fileJsonName } = CONSTANT;
+
 class App extends Component {
   state = {
     data: [],
     providerData: [],
     isLoading: true,
+    errorMsg: null,
   };
+
 
   componentDidMount() {
     auth.getAuth().onAuthStateChanged(user => {
@@ -28,19 +32,31 @@ class App extends Component {
     });
 
     fetch(`${sourceJsonURL}/${fileJsonName}`)
-    .then(res => res.json())
-    .then((data) => {this.setState({ data: data })});
+      .then(res => {
+        if (!res.ok) { throw res }
+        return res.json()
+      })
+      .then((data) => {
+        this.setState({ data: data })
+      })
+      .catch((err) => {
+        this.setState({ errorMsg: `Can't load data` })
+      });
   }
 
   render() {
     return this.state.isLoading ? <Preloader /> : (
-      this.state.providerData.length > 0 ?
-        (<Dashboard
+      this.state.providerData.length > 0 && this.state.data.length > 0 ?
+        <Dashboard
           {...this.props}
           providerData={this.state.providerData}
           data={this.state.data}
-        /> )
-        : (<Login />)
+        />:
+
+      this.state.errorMsg === null ? <Login />:
+        <Layout contentTitle={ this.state.errorMsg } contentCenter={true}>
+          Please try again later...
+        </Layout>
     )
   }
 }
